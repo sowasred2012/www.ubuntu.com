c0ba0111933c43aaf344edc4e61bcac9889e9bf9
Scott Mason Nash <sowasred2012@gmail.com>
2020-04-08 17:28:45 +0100

    fix contract endpoint params

--- webapp/app.py
+++ webapp/app.py
@@ -31,6 +31,7 @@ from webapp.context import (
     get_navigation,
     releases,
 )
+
 from webapp.views import (
     accept_renewal,
     advantage_view,
@@ -121,11 +122,11 @@ app.add_url_rule(
     methods=["POST"],
 )
 app.add_url_rule(
-    "/advantage/renewals/{renewal_id}", view_func=get_renewal, methods=["GET"]
+    "/advantage/renewals/<renewal_id>", view_func=get_renewal, methods=["GET"],
 )
 
 app.add_url_rule(
-    "/advantage/renewals/{renewal_id}/process-payment",
+    "/advantage/renewals/<renewal_id>/process-payment",
     view_func=accept_renewal,
     methods=["POST"],
 )
